04efce996a2e53bb5ba737d5f3321de33c16f549
ezamoram <ezamoram@pichincha.com>
2024-01-10 08:28:19 -0500

    Add first project

--- my-first-app/src/exercice/First.js
+++ my-first-app/src/exercice/First.js
@@ -2,7 +2,9 @@ function FirstApp(params) {
   return embeddingExpressions;
 }
 
-const name = "Edwin Zamora";
-const embeddingExpressions = <h1> Hello First line, {name}</h1>;
+const name = "Edwin ";
+const lastName="Zamora"
+const secondlastName="Xaqwert"
+const embeddingExpressions = <h1> Hello First line, {name} , {lastName} , {secondlastName}</h1>;
 
 export default FirstApp;
--- my-first-app/src/reactDev/Responding.js
+++ my-first-app/src/reactDev/Responding.js
@@ -1,8 +1,8 @@
 function MyButton() {
   function handleClick() {
-    alert("You clicked me!");
+    alert("You clicked me!  I'm Back ");
   }
-  return <button onClick={handleClick}>Click me</button>;
+  return <button onClick={handleClick}>Click me !..</button>;
 }
 
 export default MyButton;
